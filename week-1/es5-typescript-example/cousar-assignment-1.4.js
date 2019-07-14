13590459b31a92f8bcc7bc2771585ea9c4174d46
Donald Cousar <drcousar@yahoo.com>
2019-07-13 22:04:13 -0500

    Add assignment 1.4

---
+++ week-1/es5-typescript-example/cousar-assignment-1.4.js
@@ -0,0 +1,16 @@
+/**
+ * ===========================
+ * Title: TypeScript
+ * Name: cousar-assignment-1.4
+ * Author: Donald Cousar
+ * Date: 7/13/2019
+ * ===========================
+ */
+//who am i
+var myvar1 = "Don";
+var myvar2 = "Cousar";
+function myName(myParam1, myParam2) {
+    return ('Hello ' + myParam1 + ' ' + myParam2);
+}
+//return name
+console.log(myName(myvar1, myvar2));
---
+++ week-1/es5-typescript-example/cousar-assignment-1.4.ts
@@ -0,0 +1,20 @@
+/**
+ * ===========================
+ * Title: TypeScript
+ * Name: cousar-assignment-1.4
+ * Author: Donald Cousar
+ * Date: 7/13/2019
+ * ===========================
+ */
+
+ //who am i
+var myvar1 = "Don";
+var myvar2 = "Cousar";
+
+function myName (myParam1: string, myParam2: string) {
+    return ('Hello ' + myParam1 + ' ' + myParam2);
+}
+
+//return name
+console.log(myName(myvar1, myvar2));
+
