ccf7dc020940b2de1cbf4bcd90ae8d8c6889bd79
Donald Cousar <drcousar@yahoo.com>
2019-09-09 02:56:02 -0500

    updating bobs computer

--- week-9/cousar-bobs-computer/src/app/app.module.ts
+++ week-9/cousar-bobs-computer/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import {MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +22,14 @@ import {MatListModule} from '@angular/material/list';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatListModule
+    MatListModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
